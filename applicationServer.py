7e56ac5109839e2e510029964069437265793cd4
Benjamin Westburg <38194972+westbenj2020@users.noreply.github.com>
2021-04-13 22:53:30 -0400

    04_13_2021
    Note: Code has been restructured to exclude AES implementations; DES is now used as specified in the assignment specifications. Naming conventions have been revised, however, there exists a need to induce complete uniformity across source files. The first and second phases - application server and client registration - are implemented and need only to be tested. The third phase has yet to be implemented. A JPG file has been included in this commitment that delineates the various phases in prescribed steps; this message flow diagram can be useful in understanding, with more granularity, the nature of the project.

--- applicationServer.py
+++ applicationServer.py
@@ -8,6 +8,8 @@ import hashlib
 import hmac
 import time
 from Crypto.Random import get_random_bytes
+import random
+import string
 
 #  network configuration information
 
@@ -21,13 +23,13 @@ centralizedCertificateAuthority_id = 'ID-CA'
 sessionLifetime = 86400
 
 #  temporary DES key retrieval -> application server
-applicationServer_tk_file = open('applicationServer_temp_DES_key.txt', 'r')
-applicationServer_tk = applicationServer_tk_file.read(8)
-applicationServer_tk_file.close()
+letters = string.ascii_lowercase
+applicationServer_tk = (''.join(random.choice(letters) for i in range(8)))
 print('\nTemporary DES key retrieved: "{}"'.format(applicationServer_tk))
 
 #  public-key retrieval -> centralized certificate authority
-centralizedCertificateAuthority_publicKey = RSA.import_key(open("centralizedCertificateAuthority_publicKey.pem").read())
+centralizedCertificateAuthority_publicKey = RSA.import_key(open(
+    "CCA_centralizedCertificateAuthority_publicKey.pem").read())
 
 #  connection logic
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as applicationServer:
@@ -38,47 +40,104 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as applicationServer:
     centralizedCertificateAuthority, address = applicationServer.accept()
     with centralizedCertificateAuthority:
 
-        #  Application server registration; construction and sending of data
+        #  Application server registration; construction and sending of data (1)
         #  (Application Server -> Centralized Certificate Authority)
         #  Constructed message contents:
         #  [Application Server Temporary Key1(8 chars)||ID of Application Server(9 chars)||Timestamp1(10 digits)
-        session_key = get_random_bytes(16)
         timestamp1 = time.time().__trunc__()
-        nonEncryptedMessage = applicationServer_tk.__str__() + applicationServer_id + timestamp1.__str__()
-        nonEncryptedMessageEncoded = nonEncryptedMessage.encode('utf-8')
-        file_out = open("encrypted_data.bin", "wb")
-        RSA_cipher = PKCS1_OAEP.new(centralizedCertificateAuthority_publicKey)
-        encryptedSessionKey = RSA_cipher.encrypt(session_key)
-        cipher_aes = AES.new(session_key, AES.MODE_EAX)
-        ciphertext, tag = cipher_aes.encrypt_and_digest(nonEncryptedMessageEncoded)
-        print('\nSent ciphertext: "{}"'.format(ciphertext))
-        [file_out.write(x) for x in (encryptedSessionKey, cipher_aes.nonce, tag, ciphertext)]
-        file_out.close()
-        time.sleep(5)
+        nonEncryptedMessage1 = applicationServer_tk.__str__() + applicationServer_id + timestamp1.__str__()
+        nonEncryptedMessageEncoded1 = nonEncryptedMessage1.encode("utf-8")
+        RSA_cipher1 = PKCS1_OAEP.new(centralizedCertificateAuthority_publicKey)
+        encryptedMessageEncoded1 = RSA_cipher1.encrypt(nonEncryptedMessageEncoded1)
+        print('\nSent ciphertext: "{}"'.format(encryptedMessageEncoded1))
+        centralizedCertificateAuthority.sendall(encryptedMessageEncoded1)
 
-        #  Application server registration; reception and parsing of message contents
+        #  Application server registration; reception and parsing of message contents (2)
         #  (Centralized Certificate Authority -> Application Server)
         #  Expected message contents:
         #  [Application Server Public-Key||Application Server Private-Key +\
         #  ||Certificate||Application Server ID||Timestamp2]
-
-        file_in = open("encrypted_data.bin", "rb")
-        encryptedSessionKey, nonce1, tag1, ciphertext1 = \
-            [file_in.read(x) for x in (centralizedCertificateAuthority_publicKey.size_in_bytes(), 16, 16, -1)]
-        RSA_cipher1 = PKCS1_OAEP.new(centralizedCertificateAuthority_publicKey)
-        cipher_aes1 = AES.new(session_key, AES.MODE_EAX, nonce1)
-        print('\nReceived ciphertext: "{}"'.format(ciphertext1))
-        data = cipher_aes1.decrypt_and_verify(ciphertext1, tag1)
-        dataDecoded = data.decode("utf-8")
-        receivedTimestamp2 = dataDecoded[-10:]
-        receivedApplicationServerID = dataDecoded[-19:-10]
-        receivedApplicationServerPublicKey = dataDecoded[0:461]
-        receivedApplicationServerPrivateKey = dataDecoded[461:2164]
+        receivedData2 = centralizedCertificateAuthority.recv(4096)
+        des_cipher2 = DES.new(applicationServer_tk.encode("utf-8"), DES.MODE_OFB)
+        receivedDataDecrypted2 = des_cipher2.decrypt(receivedData2)
+        print('\nReceived ciphertext: "{}"'.format(receivedData2))
+        receivedDataDecryptedDecoded2 = receivedDataDecrypted2.decode("utf-8")
+        receivedTimestamp2 = receivedDataDecryptedDecoded2[-10:]
+        receivedApplicationServerID = receivedDataDecryptedDecoded2[-19:-10]
+        upperBoundApplicationServerPublicKey = receivedDataDecryptedDecoded2.rindex("END PUBLIC KEY-----") + 17
+        lowerBoundApplicationServerPublicKey = receivedDataDecryptedDecoded2.find("-----BEGIN PUBLIC KEY")
+        receivedApplicationServerPublicKey =\
+            receivedDataDecryptedDecoded2[lowerBoundApplicationServerPublicKey:upperBoundApplicationServerPublicKey]
+        upperBoundApplicationServerPrivateKey = receivedDataDecryptedDecoded2.rindex("END PRIVATE KEY-----") + 17
+        lowerBoundApplicationServerPrivateKey = receivedDataDecryptedDecoded2.find("-----BEGIN PRIVATE KEY")
+        receivedApplicationServerPrivateKey =\
+            receivedDataDecryptedDecoded2[lowerBoundApplicationServerPrivateKey:upperBoundApplicationServerPrivateKey]
         print('\nReceived public-key for independent usage: "{}"'.format(receivedApplicationServerPublicKey))
         print('\nReceived private-key for independent usage: "{}"'.format(receivedApplicationServerPrivateKey))
-        certificate = dataDecoded.replace(receivedTimestamp2, '').replace(receivedApplicationServerPublicKey, '').\
+        certificate = receivedDataDecryptedDecoded2.replace(receivedTimestamp2, '')\
+            .replace(receivedApplicationServerPublicKey, '').\
             replace(receivedApplicationServerPrivateKey, '').replace(receivedApplicationServerID, '')
-        print('\n certificate = "{}"'.format(certificate))
+        print('\nReceived certificate = "{}"'.format(certificate))
+
+        #
+
+        #  Connecting with client for phase two of protocol
+        client, clientAddress = applicationServer.accept()
+        with client:
+
+            #  Client Registration; reception of message contents (3)
+            #  (Client -> Application Server)
+            #  Expected message contents:
+            #  [Application Server ID||Timestamp3]
+            receivedData3 = client.recv(4096)
+            print('\nReceived plaintext from client: "{}"'.format(receivedData3.decode("utf-8")))
+
+            #  Client Registration; construction and sending of message contents (4)
+            #  (Application Server -> Client)
+            #  Constructed message contents:
+            #  [Application Server Public-Key||Certificate||Timestamp4]
+            timestamp4 = time.time().__trunc__()
+            message4 = receivedApplicationServerPublicKey + certificate + timestamp4.__str__()
+            encodedMessage4 = message4.encode("utf-8")
+            client.sendall(encodedMessage4)
+
+            #  Client Registration; second reception - and parsing - of message contents (5)
+            #  (Client -> Application Server)
+            #  Expected message contents:
+            #  [Temporary Key2||Client ID||Client IP Address||Client Port||Timestamp5]
+            receivedData5 = client.recv(4096)
+            print('\nReceived ciphertext from client: "{}"'.format(receivedData5))
+
+            #  Application server public-key importation
+            applicationServer_privateKey = RSA.import_key(receivedApplicationServerPrivateKey)
+
+            RSA_cipher5 = PKCS1_OAEP.new(applicationServer_privateKey)
+            decryptedMessageEncoded5 = RSA_cipher5.decrypt(receivedData5)
+            decryptedMessageDecoded5 = decryptedMessageEncoded5.decode("utf-8")
+            upperBoundTempKeyExtract5 = decryptedMessageDecoded5.find('ID-')
+            extractedTempKey5 = decryptedMessageDecoded5[0:upperBoundTempKeyExtract5]
+            print('\nReceived temporary DES key from client: "{}"'.format(extractedTempKey5))
+
+            #  Client Registration; construction and sending of message contents (6)
+            #  (Application Server -> Client)
+            #  Constructed message contents:
+            #  [Session Key||Session Lifetime||Client ID||Timestamp6]
+            letters = string.ascii_lowercase
+            session_key = (''.join(random.choice(letters) for k in range(8)))
+            print('\nGenerated session key: "{}"'.format(session_key))
+            timestamp6 = time.time().__trunc__()
+            message6 = session_key.__str__() + sessionLifetime.__str__() + client1_id + timestamp6.__str__()
+            message6Encoded = message6.encode("utf-8")
+            cipher6 = DES.new(extractedTempKey5.encode("utf-8"), DES.MODE_OFB)
+            message6EncodedEncrypted = cipher6.encrypt(message6Encoded)
+            client.sendall(message6EncodedEncrypted)
+            print('\nSent ciphertext to client: "{}"'.format(message6EncodedEncrypted))
+
+
+
+
+
+
 
 
 
--- client1.py
+++ client1.py
@@ -1,9 +1,15 @@
 import socket
-from Crypto.Cipher import DES
+from Crypto.Cipher import DES, AES, PKCS1_OAEP
+from Crypto.PublicKey import RSA
+from Crypto.Signature import pkcs1_15
+from Crypto.Hash import SHA256
 from Crypto.Util.Padding import pad, unpad
 import hashlib
 import hmac
 import time
+from Crypto.Random import get_random_bytes
+import string
+import random
 
 HOST = '127.0.0.1'  # IP address for server socket
 PORT = 65432  # port for server socket
@@ -15,3 +21,75 @@ centralizedCertificateAuthority_id = 'ID-CA'
 req = 'memo'
 data = 'take cis3319 class this morning'
 sessionLifetime = 86400
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as applicationServer:
+    applicationServer.connect((HOST, PORT))
+    print('\nClient is running...')
+    print('\nConnected to application server using IP address "{}" - port "{}"...'.format(HOST, PORT))
+
+    #  Client Registration; construction and sending of message content (3)
+    #  (Client -> Application Server)
+    #  Constructed message contents:
+    #  [Application Server ID||Timestamp3]
+    timestamp3 = time.time().__trunc__()
+    message3 = applicationServer_id + timestamp3.__str__()
+    encodedMessage3 = message3.encode("utf-8")
+    applicationServer.sendall(encodedMessage3)
+
+    #  Client Registration; reception and parsing of message content (4)
+    #  (Application Server -> Client)
+    #  Expected message contents:
+    #  [Application Server Public-Key||Certificate||Timestamp4]
+    receivedData4 = applicationServer.recv(4096)
+    receivedDataDecoded4 = receivedData4.decode("utf-8")
+    receivedTimestamp4 = receivedDataDecoded4[-10:]
+    upperBoundApplicationServerPublicKey = receivedDataDecoded4.rindex("END PUBLIC KEY-----") + 17
+    lowerBoundApplicationServerPublicKey = receivedDataDecoded4.find("-----BEGIN PUBLIC KEY")
+    receivedApplicationServerPublicKey = \
+        receivedDataDecoded4[lowerBoundApplicationServerPublicKey:upperBoundApplicationServerPublicKey]
+    print('\nReceived plaintext from application server: "{}"'.format(receivedDataDecoded4))
+    certificate = receivedDataDecoded4.replace(receivedTimestamp4, '').replace(receivedApplicationServerPublicKey, '')
+
+    #  Verification of received application server public-key and certificate
+
+    #  Client Registration; second construction and sending of message content (5)
+    #  (Client -> Application Server)
+    #  Constructed message contents:
+    #  [Temporary Key2||Client ID||Client IP Address||Client Port||Timestamp5]
+    letters = string.ascii_lowercase
+    client_tk = (''.join(random.choice(letters) for i in range(8)))
+    print('\nSecond temporary-key generated: "{}"'.format(client_tk))
+    timestamp5 = time.time().__trunc__()
+    message5 = client_tk + client1_id + HOST + PORT.__str__() + timestamp5.__str__()
+    encodedMessage5 = message5.encode("utf-8")
+
+    #  Application server public-key importation
+    applicationServer_publicKey = RSA.import_key(receivedApplicationServerPublicKey)
+
+    RSA_cipher5 = PKCS1_OAEP.new(applicationServer_publicKey)
+    encryptedMessageEncoded5 = RSA_cipher5.encrypt(encodedMessage5)
+    applicationServer.sendall(encryptedMessageEncoded5)
+    print('\nSent ciphertext to application server: "{}"'.format(encryptedMessageEncoded5))
+
+    #  Client Registration; second reception and parsing of message content (6)
+    #  (Application Server -> Client)
+    #  Expected message contents:
+    #  [Session Key||Session Lifetime||Client ID||Timestamp6]
+    receivedData6 = applicationServer.recv(4096)
+    des_cipher6 = DES.new(client_tk.encode("utf-8"), DES.MODE_OFB)
+    receivedDataDecrypted6 = des_cipher6.decrypt(receivedData6)
+    print('\nReceived ciphertext from application server: "{}"'.format(receivedData6))
+    receivedDataDecryptedDecoded6 = receivedDataDecrypted6.decode("utf-8")
+    upperBoundSessionKeyExtract6 = receivedDataDecryptedDecoded6.find('86400')
+    extractedSessionKey6 = receivedDataDecryptedDecoded6[0:upperBoundSessionKeyExtract6]
+    print('\nReceived session key from application server: "{}"'.format(extractedSessionKey6))
+
+
+
+
+
+
+
+
+
+
