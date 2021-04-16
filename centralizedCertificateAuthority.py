300d0d2d0965c3e7b59fe360163084b25a6167c6
Benjamin Westburg <38194972+westbenj2020@users.noreply.github.com>
2021-04-15 21:30:37 -0400

    04_15_2021
    Note: This repository commit serves as the final version in the project's developmental iterations; there will be no further updates. One drawback of the implementation is that private keys are stored in globally accessible files. For this reason, the application server and client simply open these respective files to import desired keys. This configuration is partly due to the fact that the pycryptodome library seems to have issues dealing with the importation of keys outside of the generating program. If this project is to be further pursued, it would be suggested that the centralized certificate authority, which generates and stores all RSA keys, restrict access to files containing private keys. Another circumvention would involve the sending of files containing private keys, as determining access to a given private key is already approved for an entity with or without this addition. That is, we can either send text, which the receiving entity will have to parse, or much more simply, we can just send the file containing the private key that has been created for that particular entity.

--- centralizedCertificateAuthority.py
+++ centralizedCertificateAuthority.py
@@ -13,25 +13,26 @@ from Crypto.Random import get_random_bytes
 HOST = '127.0.0.1'  # IP address for server socket
 PORT = 65432  # port for server socket
 networkAddress = HOST + PORT.__str__()
-BLOCK_SIZE = 256
+BLOCK_SIZE = 32
 client1_id = 'ID-Client1'
 applicationServer_id = 'ID-Server'
 centralizedCertificateAuthority_id = 'ID-CA'
-centralizedCertificateAuthority_sk = 'radiance'  # private key
 sessionLifetime = 86400
 
 #  key pair generation (private and public) -> centralized certificate authority
 centralizedCertificateAuthority_keyPair = RSA.generate(2048)
 centralizedCertificateAuthority_privateKey = centralizedCertificateAuthority_keyPair.export_key()
-file_out = open("centralizedCertificateAuthority_privateKey.pem", "wb")
+file_out = open("CCA_centralizedCertificateAuthority_privateKey.pem", "wb")
 file_out.write(centralizedCertificateAuthority_privateKey)
 file_out.close()
 centralizedCertificateAuthority_publicKey = centralizedCertificateAuthority_keyPair.publickey().export_key()
-file_out = open("centralizedCertificateAuthority_publicKey.pem", "wb")
+file_out = open("CCA_centralizedCertificateAuthority_publicKey.pem", "wb")
 file_out.write(centralizedCertificateAuthority_publicKey)
 file_out.close()
 
-time.sleep(5)
+#  private-key retrieval -> centralized certificate authority
+centralizedCertificateAuthority_privateKey =\
+    RSA.import_key(open("CCA_centralizedCertificateAuthority_privateKey.pem").read())
 
 #  connection logic
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as applicationServer:
@@ -39,62 +40,57 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as applicationServer:
     print('\nCentralized certificate authority is running...')
     print('\nConnected to application server using IP address "{}" - port "{}"...'.format(HOST, PORT))
 
-    #  Application server registration; reception and parsing of data
+    #  Application server registration; reception and parsing of data (1)
     #  (Application Server -> Centralized Certificate Authority)
     #  Expected message contents:
     #  [Application Server Temporary Key1(8 chars)||ID of Application Server(9 chars)||Timestamp1(10 digits)
-    file_in = open("encrypted_data.bin", "rb")
-    centralizedCertificateAuthority_privateKey =\
-        RSA.import_key(open("centralizedCertificateAuthority_privateKey.pem").read())
-    encryptedSessionKey, nonce, tag, ciphertext =\
-        [file_in.read(x) for x in (centralizedCertificateAuthority_privateKey.size_in_bytes(), 16, 16, -1)]
-    RSA_cipher = PKCS1_OAEP.new(centralizedCertificateAuthority_privateKey)
-    session_key = RSA_cipher.decrypt(encryptedSessionKey)
-    cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
-    print('\nReceived ciphertext: "{}"'.format(ciphertext))
-    data = cipher_aes.decrypt_and_verify(ciphertext, tag)
-    dataDecoded = data.decode("utf-8")
-    receivedApplicationServer_temp_DES_key = dataDecoded[0:8]
-    print('\nTemporary DES key extracted from application server: "{}"'.format(receivedApplicationServer_temp_DES_key))
-    receivedApplicationServer_id = dataDecoded[8:17]
-    receivedApplicationServer_timestamp = dataDecoded[17:27]
+    receivedData1 = applicationServer.recv(4096)
+    RSA_cipher1 = PKCS1_OAEP.new(centralizedCertificateAuthority_privateKey)
+    receivedDataDecrypted1 = RSA_cipher1.decrypt(receivedData1)
+    print('\nReceived ciphertext from application server: "{}"'.format(receivedData1))
+    receivedDataDecryptedDecoded1 = receivedDataDecrypted1.decode("utf-8")
+    upperBoundTempKeyExtract1 = receivedDataDecryptedDecoded1.find('ID-')
+    extractedTempKey1 = receivedDataDecryptedDecoded1[0:upperBoundTempKeyExtract1]
+    print('\nExtracted temporary key from application server: "{}"'.format(extractedTempKey1))
 
     #  Generation of public and private key to be provided to the application server for independent usage
     applicationServer_keyPair = RSA.generate(2048)
     applicationServer_privateKey = applicationServer_keyPair.export_key()
-    file_out = open("applicationServer_privateKey.pem", "wb")
+    file_out = open("CCA_applicationServer_privateKey.pem", "wb")
     file_out.write(applicationServer_privateKey)
     file_out.close()
     applicationServer_publicKey = applicationServer_keyPair.publickey().export_key()
-    file_out = open("applicationServer_publicKey.pem", "wb")
+    file_out = open("CCA_applicationServer_publicKey.pem", "wb")
     file_out.write(applicationServer_publicKey)
     file_out.close()
     print('\nPublic-key generated for application server: "{}"'.format(applicationServer_publicKey.__str__()))
     print('\nPrivate-key generated for application server: "{}"'.format(applicationServer_privateKey.__str__()))
 
-    #  Application server registration; certificate generation
+    #  Application server registration; certificate generation (2)
     message = applicationServer_id + centralizedCertificateAuthority_id + applicationServer_publicKey.__str__()
     h = SHA256.new(message.encode("utf-8"))
     signature = pkcs1_15.new(centralizedCertificateAuthority_privateKey).sign(h)
-    RSA_cipher = PKCS1_OAEP.new(centralizedCertificateAuthority_privateKey)
+    RSA_cipher2 = PKCS1_OAEP.new(centralizedCertificateAuthority_privateKey)
     certificate = signature
     print('\nCertificate generated for application server: "{}"'.format(certificate.__str__()))
 
-    #  Application server registration; construction and sending of message contents
+    #  Application server registration; construction and sending of message contents (2)
     #  (Centralized Certificate Authority -> Application Server)
     #  Constructed message contents:
     #  [Application Server Public-Key||Application Server Private-Key||Certificate||Application Server ID||Timestamp2]
-    timestamp = time.time().__trunc__()
-    nonEncryptedMessage = applicationServer_publicKey.__str__() + applicationServer_privateKey.__str__() +\
-        certificate.__str__() + applicationServer_id + timestamp.__str__()
-    nonEncryptedMessageEncoded = nonEncryptedMessage.encode("utf-8")
-    cipher_aes1 = AES.new(session_key, AES.MODE_EAX, nonce)
-    ciphertext1, tag1 = cipher_aes1.encrypt_and_digest(nonEncryptedMessageEncoded)
-    file_out = open("encrypted_data.bin", "wb")
-    file_out.flush()
-    [file_out.write(x) for x in (encryptedSessionKey, cipher_aes1.nonce, tag1, ciphertext1)]
-    file_out.close()
-    print('\nSent ciphertext: "{}"'.format(ciphertext1))
+    timestamp2 = time.time().__trunc__()
+    nonEncryptedMessage2 = applicationServer_publicKey.__str__() + applicationServer_privateKey.__str__() +\
+        certificate.__str__() + applicationServer_id + timestamp2.__str__()
+    nonEncryptedMessageEncoded2 = nonEncryptedMessage2.encode("utf-8")
+    cipher2 = DES.new(extractedTempKey1.encode('utf-8'), DES.MODE_ECB)
+    encryptedMessageEncoded2 = cipher2.encrypt(pad(nonEncryptedMessageEncoded2, BLOCK_SIZE))
+    print('\nTEST >>> "{}'.format(nonEncryptedMessageEncoded2))
+    applicationServer.sendall(encryptedMessageEncoded2)
+    print('\nSent ciphertext to application server: "{}"'.format(encryptedMessageEncoded2))
+    print('\nCentralized certificate authority will now disconnect from the application server...')
+
+
+
 
 
 
